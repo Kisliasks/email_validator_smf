3b75a94a9524f820148c42d852507f9ab595d0bc
dkislyak <dkislyak@team.amocrm.com>
2023-04-07 17:35:25 +0300

    regex service edits

--- src/Shared/Service/Mods/RegexValidateService.php
+++ src/Shared/Service/Mods/RegexValidateService.php
@@ -2,31 +2,16 @@
 
 declare(strict_types=1);
 
-namespace App\Shared\Domain\Service\Validator\EmailValidator;
+namespace App\Shared\Service\Mods;
 
-use App\Shared\Application\Interfaces\BaseValidateInterface;
 use App\Shared\Application\Interfaces\RegexValidateInterface;
+use App\Shared\Domain\Service\Validator\EmailValidator\BaseValidateService;
 
-class RegexValidateService extends BaseValidateService implements BaseValidateInterface, RegexValidateInterface
+/**
+ * Расширяет базовый класс валидации
+ */
+class RegexValidateService extends BaseValidateService implements RegexValidateInterface
 {
-    /**
-     * @param string $email
-     * @return bool
-     */
-    public function validateWithSpamDataBase(string $email): bool
-    {
-        return $this->validateWithSpamDataBase($email);
-    }
-
-    /**
-     * @param string $email
-     * @return bool
-     */
-    public function validateWithPost(string $email): bool
-    {
-        return $this->validateWithPost($email);
-    }
-
     /**
      * @param string $email
      * @return bool
